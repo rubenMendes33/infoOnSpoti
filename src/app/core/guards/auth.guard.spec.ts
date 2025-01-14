5289496abc88ca78f643f413058b3fbed8e75ec1
rfmendes <rfmendes@criticalsoftware.com>
2025-01-14 14:36:07 +0000

    fixed all component tests

--- src/app/core/guards/auth.guard.spec.ts
+++ src/app/core/guards/auth.guard.spec.ts
@@ -1,17 +1,55 @@
 import { TestBed } from '@angular/core/testing';
-import { CanActivateFn } from '@angular/router';
-import { authGuard } from './auth.guard';
-
+import { AuthGuard } from './auth.guard';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('AuthGuard', () => {
-  const executeGuard: CanActivateFn = (...guardParameters) =>
-      TestBed.runInInjectionContext(() => authGuard(...guardParameters));
+  let guard: AuthGuard;
+  let angularFireAuthMock: Partial<AngularFireAuth>;
+  let routerMock: jest.Mocked<Router>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    routerMock = {
+      navigate: jest.fn(),
+    } as unknown as jest.Mocked<Router>;
+
+    angularFireAuthMock = {
+      get authState() {
+        return of(null); // Default to unauthenticated state
+      },
+    };
+
+    TestBed.configureTestingModule({
+      providers: [
+        AuthGuard,
+        { provide: AngularFireAuth, useValue: angularFireAuthMock },
+        { provide: Router, useValue: routerMock },
+      ],
+    });
+
+    guard = TestBed.inject(AuthGuard);
   });
 
-  it('should be created', () => {
-    expect(executeGuard).toBeTruthy();
+  it('should allow activation when user is authenticated', (done) => {
+    // Mock authenticated state
+    jest.spyOn(angularFireAuthMock, 'authState', 'get').mockReturnValue(of({ uid: '12345' } as any));
+
+    guard.canActivate().subscribe((canActivate) => {
+      expect(canActivate).toBe(true); // Authenticated user can activate the route
+      expect(routerMock.navigate).not.toHaveBeenCalled(); // No navigation to /login
+      done();
+    });
+  });
+
+  it('should deny activation when user is not authenticated', (done) => {
+    // Mock unauthenticated state
+    jest.spyOn(angularFireAuthMock, 'authState', 'get').mockReturnValue(of(null));
+
+    guard.canActivate().subscribe((canActivate) => {
+      expect(canActivate).toBe(false); // Unauthenticated user cannot activate the route
+      expect(routerMock.navigate).toHaveBeenCalledWith(['/login']); // Redirect to /login
+      done();
+    });
   });
 });
