f8519d4e4eccc1fdc7fc84bb4e1050236521f1fd
rfmendes <rfmendes@criticalsoftware.com>
2025-01-14 12:38:53 +0000

    fixed all component tests

--- src/app/shared/services/toast.service.spec.ts
+++ src/app/shared/services/toast.service.spec.ts
@@ -1,12 +1,20 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ToastService } from './toast.service';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {FakeLoader} from '../../app.component.spec';
 
 describe('ToastService', () => {
   let service: ToastService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        TranslateModule.forRoot({
+          loader: { provide: TranslateLoader, useClass: FakeLoader }
+        })
+      ],
+    });
     service = TestBed.inject(ToastService);
   });
 
