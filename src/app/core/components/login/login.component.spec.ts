ec46d18d67b6228ad0808991c513ea589cb13525
rfmendes <rfmendes@criticalsoftware.com>
2025-01-15 15:42:15 +0000

    fixed all component tests

--- src/app/core/components/login/login.component.spec.ts
+++ src/app/core/components/login/login.component.spec.ts
@@ -1,4 +1,4 @@
-import {render, screen } from '@testing-library/angular';
+import {fireEvent, render, screen} from '@testing-library/angular';
 import {LoginComponent} from './login.component';
 import {DeferBlockState, TestBed} from '@angular/core/testing';
 import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
@@ -6,6 +6,10 @@ import {FakeLoader} from '../../../app.component.spec';
 import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
 import {getAuth, provideAuth} from '@angular/fire/auth';
 import '@testing-library/jest-dom'
+import { AuthService } from '../../services/auth.service';
+import { ToastService } from '../../../shared/services/toast.service';
+import { Router } from '@angular/router';
+import {of} from 'rxjs';
 
 
 
@@ -20,8 +24,27 @@ const mockFirebaseConfig = {
 };
 
 describe('LoginComponent', () => {
+  let authServiceMock: AuthService;
+  let toastService: ToastService;
+  let router: Router;
 
   beforeEach(async () => {
+    authServiceMock = {
+      login: () => of(null)
+    } as unknown as AuthService;
+
+    toastService = {
+      showSuccess: jest.fn(),
+      showError: jest.fn()
+    } as unknown as ToastService;
+
+    router = {
+      navigateByUrl: () => new Promise(() => {}),
+      routerState: {
+        root: {}
+      }
+    } as unknown as Router;
+
     await TestBed.configureTestingModule({
       imports: [
         LoginComponent,
@@ -31,22 +54,53 @@ describe('LoginComponent', () => {
       ],
       providers:[
         provideFirebaseApp(() => initializeApp(mockFirebaseConfig)),
-        provideAuth(() => getAuth())
+        provideAuth(() => getAuth()),
+        { provide: AuthService, useFactory: () => authServiceMock },
+        { provide: ToastService, useValue: toastService },
+        { provide: Router, useValue: router },
       ]
     })
       .compileComponents();
   });
+
   it('should render login form', async () => {
     const { fixture } = await render(LoginComponent);
     const deferBlockFixture = (await fixture.getDeferBlocks())[0];
     await deferBlockFixture.render(DeferBlockState.Complete);
 
     const emailInput = screen.getByPlaceholderText('Email');
-    const passwordInput = screen.getAllByPlaceholderText('Password')[0];
+    const passwordInput = screen.getByTestId('password-input');
     const submitButton = screen.getByRole('button', { name: /login/i });
 
     expect(emailInput).toBeInTheDocument();
     expect(passwordInput).toBeInTheDocument();
     expect(submitButton).toBeInTheDocument();
   });
+
+  it('should call login method on form submit', async () => {
+    const { fixture } = await render(LoginComponent);
+    const deferBlockFixture = (await fixture.getDeferBlocks())[0];
+    await deferBlockFixture.render(DeferBlockState.Complete);
+
+    const spy = jest.spyOn(authServiceMock, 'login');
+
+    const emailInput = screen.getByPlaceholderText('Email');
+    const passwordInput = screen.getByTestId('password-input');
+    const submitButton = screen.getByRole('button', { name: /login/i });
+
+    fireEvent.input(emailInput, { target: { value: 'test@example.com' } });
+
+    const passwordNativeInput = passwordInput.querySelector('input');
+    if (!passwordNativeInput) {
+      throw new Error('Password input not found');
+    }
+
+    // Simulate typing into the password input
+    fireEvent.input(passwordNativeInput, { target: { value: 'password123' } });
+    fireEvent.click(submitButton);
+
+    expect(spy).toHaveBeenCalledWith('test@example.com', 'password123');
+
+  });
+
 });
